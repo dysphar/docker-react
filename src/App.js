13f3745c6dc20edea877bf29dff397440c5e33f6
Oleg Krol <dysphar@gmail.com>
2019-09-29 22:20:01 +0300

    feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hah
+          Super new name!
         </a>
       </header>
     </div>
